6aa8279bd6aa887d09fa1fa0e0ccc867d6de39e5
Nikhil Gupta <nikhil062000@gmail.com>
2020-12-26 21:14:07 +0530

    necessary changes for setup

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,7 @@ login.login_view = 'login'
 from app import routes, models, errors
 
 if not app.debug:
+    #To mail 
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -36,7 +37,8 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-
+    
+    #To store in log file
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/logD.log', maxBytes=10240,
